2accf8f7187db86c931c51ce03b0ed91db8dbe9d
goyalshivani <shibidrj@gmail.com>
2024-08-28 11:57:37 +0530

    Update deploy-webapp-try.ps1

--- scripts/deploy-webapp-try.ps1
+++ scripts/deploy-webapp-try.ps1
@@ -3,6 +3,6 @@ $location="North Europe"
 $appServiceName="newservicename098723476528765"
 $webAppName="webapp09873376549987"
  
-New-AzAppServicePlan -ResourceGroupName $resourceGroup -Location $location -Name $appServiceName -Tier "F1"
+New-AzAppServicePlan -ResourceGroupName $resourceGroup -Location $location -Name $appServiceName -Tier "PremiumV3"
  
 New-AzWebApp -ResourceGroupName $resourceGroup -Name $webAppName -Location $location -AppServicePlan $appServiceName
